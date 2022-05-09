b6c77d5437b38740f9e532928bb7412c86ec34cb
DeAntoine <47144615+DeAntoine@users.noreply.github.com>
2022-05-09 12:11:25 +0200

    nouvelles activités

---
+++ app/src/main/java/yummy_dvice/com/SignInPage2Activity.java
@@ -0,0 +1,14 @@
+package yummy_dvice.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInPage2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_page2);
+    }
+}
