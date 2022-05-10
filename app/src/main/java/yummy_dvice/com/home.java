f9c1bed0f485ac20d46c6e6d9f7d6786e5092d69
HugoD98 <hugo.ducly@gmail.com>
2022-05-10 17:24:44 +0200

    Add grid to display restaurants (horriblement chiant j'ai envie de crever)

--- app/src/main/java/yummy_dvice/com/home.java
+++ app/src/main/java/yummy_dvice/com/home.java
@@ -1,27 +1,69 @@
 package yummy_dvice.com;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.gridlayout.widget.GridLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import yummy_dvice.com.databinding.HomeBinding;
+
 public class home extends AppCompatActivity {
+
+    HomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
 
-        BottomNavigationView navBar = findViewById(R.id.bottom_nav);
+        binding = HomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.home);
 
+        // get restaurant name and image
+        String[] flowerName = {"Rose","Lotus","Lily","Jasmine",
+                "Tulip","Orchid","Levender","RoseMarry","Sunflower","Carnation"};
+        int[] flowerImages = {R.drawable.grec,R.drawable.grec,R.drawable.grec,R.drawable.grec,R.drawable.grec,R.drawable.grec,R.drawable.grec,R.drawable.grec,R.drawable.grec,R.drawable.grec};
+
+        GridAdapter gridAdapter = new GridAdapter(home.this,flowerName,flowerImages);
+        binding.gridView.setAdapter(gridAdapter);
+
+        binding.gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(home.this,"You Clicked on "+ flowerName[position],Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // search bar
         SearchView sv = findViewById(R.id.searchView);
 
+        // navBar
+        BottomNavigationView navBar = findViewById(R.id.bottom_nav);
         navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -33,12 +75,35 @@ public class home extends AppCompatActivity {
                     Intent home = new Intent(getApplicationContext(), LoginActivity.class);
                     startActivity(home);
                     finish();
-
                 }
-
                 return true;
             }
         });
+
+        // Map button
+        Button mapButton = findViewById(R.id.buttonMap);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent home = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(home);
+                finish();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
